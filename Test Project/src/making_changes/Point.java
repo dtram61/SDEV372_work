b990a450508f450489c0ca8c5bc8abd8c576ff61
Duc Tram <dtram61@gmail.com>
2023-01-04 11:01:57 -0800

    Added an equals() and hashcode method

--- Test Project/src/making_changes/Point.java
+++ Test Project/src/making_changes/Point.java
@@ -1,5 +1,7 @@
 package making_changes;
 
+import java.util.Objects;
+
 public class Point {
     private double x;
     private double y;
@@ -31,5 +33,22 @@ public class Point {
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
